ab3660d6ce375866cfaf48b23f76308fdac0735b
CL <1260807001@qq.com>
2018-07-24 18:07:14 +0800

    CL-add QiuBai VC

---
+++ Founction/Qiu/Controller/QiuVC.h
@@ -0,0 +1,13 @@
+//
+//  QiuVC.h
+//  MyCode
+//
+//  Created by CL on 2018/7/24.
+//  Copyright © 2018年 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiuVC : UIViewController
+
+@end
---
+++ Founction/Qiu/Controller/QiuVC.m
@@ -0,0 +1,44 @@
+//
+//  QiuVC.m
+//  MyCode
+//
+//  Created by CL on 2018/7/24.
+//  Copyright © 2018年 CL. All rights reserved.
+//
+
+#import "QiuVC.h"
+
+@interface QiuVC ()
+
+@end
+
+@implementation QiuVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    [self initWithNetData];
+}
+
+- (void)initWithNetData {
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
